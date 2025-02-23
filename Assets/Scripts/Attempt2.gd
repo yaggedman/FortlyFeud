38d9cd8c086c6d1730e0fb27a0f0e9ac117a3fdd
yaggedman <jay.hallett03@gmail.com>
2025-02-23 22:45:23 +0000

    Game now knows which piece is on which tile,
    just need to figure out how to append that tile's position to array and assign it a value relative to the piece

--- Assets/Scripts/Attempt2.gd
+++ Assets/Scripts/Attempt2.gd
@@ -3,7 +3,15 @@ extends Node2D
 #each tile is 216x216 pixels
 
 
-var GameBoardArray: Array = [ # this represents the game board at the beginning of the game. The 4s represents empty spaces on the board
+var FriendlyGameBoardArray: Array = [ # this represents the game board from the POV of the player at the beginning of the game. The 0s represents empty spaces on the board
+	[0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0],
+	[0, 0, 0, 0 ,0]
+]
+
+var EnemyGameBoardArray: Array = [ # this represents the game board from the POV of the enemy at the beginning of the game. The 0s represents empty spaces on the board
 	[0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0],
@@ -52,8 +60,8 @@ var HoldingItem: bool = false
 
 func _ready():
 	## gets buttons in the button group, and connects the pressed signal with argument button
-	for button in get_tree().get_nodes_in_group("TileButtons"):
-		button.pressed.connect(Callable(self, "_on_tile_button_pressed").bind(button))
+	for tilebutton in get_tree().get_nodes_in_group("TileButtons"):
+		tilebutton.pressed.connect(Callable(self, "_on_tile_button_pressed").bind(tilebutton))
 		
 ## gets buttons in the menupieces group and connects the pressed signal with argument menupiece
 	for menupiece in get_tree().get_nodes_in_group("MenuPieces"):
@@ -62,14 +70,25 @@ func _ready():
 	for PieceSelectionKey in PieceSelectionCheck.keys(): # sets all pieces to be deselected on game start-up
 		PieceSelectionCheck[PieceSelectionKey] = false
 		
-func _on_menupiece_button_pressed(menupiece): # true for all menu pieces
+func _on_menupiece_button_pressed(menupiece): # true for all menu pieces, when menu piece selected
 	if HoldingItem == false: # only runs code if hand is empty
-		print(menupiece.name, " pressed! Waiting for tile selection or discard")
-		for Piece in PieceSelectionCheck:
-			if PieceSelectionCheck.has(menupiece.name):
 				PieceSelectionCheck[menupiece.name] = true
 				print(PieceSelectionCheck)
-				HoldingItem = true
+		print(menupiece.name, " pressed! Waiting for tile selection or discard") # debug - prints selected piece to console
+		for Piece in PieceSelectionCheck: # loops through all pieces
+			if PieceSelectionCheck.has(menupiece.name): # if the piece is valid
+				PieceSelectionCheck[menupiece.name] = true # select that piece
+				HoldingItem = true # set holding status to true, prevents multiple valid selections
 			else:
-				print(menupiece, " not found in dictionary") # debug
-			
+				print(menupiece, " not found in dictionary") # debug, piece does not exist in the dictionary
+	print(PieceSelectionCheck) # prints all pieces and whether they are selected or not (true/false)
+
+func _on_tile_button_pressed(tilebutton):
+	print("tile ", tilebutton.name, " selected") # debug - prints selected tile to console
+	if HoldingItem == true: # only runs code if the player has a tile selected
+		for Piece in PieceSelectionCheck: # runs through all pieces, and selects the piece that has been chosen from the piece menu
+			if PieceSelectionCheck[Piece] == true:
+				print (Piece, " placed on tile ", tilebutton.name) # debug - prints name of piece and tile it has been placed on to console
+				for tile in TilesDict:
+					if TilesDict.has(tilebutton):
+						pass # append to array
